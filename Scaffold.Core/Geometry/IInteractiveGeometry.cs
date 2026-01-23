10abc7e47a6fe25e6eb2c2f1d6428e5110efc83c
Alex Baalham <a.baalham@whitbywood.com>
2026-01-23 21:26:15 +0000

    InteractiveGeometry interfaces and models added

---
+++ Scaffold.Core/Geometry/IInteractiveGeometry.cs
@@ -0,0 +1,10 @@
+﻿namespace Scaffold.Core.Geometry
+{
+    public interface IInteractiveGeometry
+    {
+        List<IInteractiveGeometryItem> InteractiveGeometryItems { get; }
+
+        List<Abstract.GeometryBase> Geometry { get; }
+
+    }
+}
