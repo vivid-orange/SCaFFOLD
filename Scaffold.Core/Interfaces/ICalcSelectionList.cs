d27bd1df1404c1175cc915c74725a718680fc6c8
Alex Baalham <a.baalham@whitbywood.com>
2026-01-27 08:09:43 +0000

    Further fixes

---
+++ Scaffold.Core/Interfaces/ICalcSelectionList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scaffold.Core
+{
+    public interface ICalcSelectionList : ICalcValue
+    {
+        List<string> Selections { get; }
+
+        int SelectedItemIndex { get; set; }
+
+        string Value { get; }
+
+    }
+}
---
+++ Scaffold.Core/Models/CalcSelectionList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Scaffold.Core.CalcValues;
+
+public class CalcSelectionList : CalcValue<string>, ICalcSelectionList
+{
+    public List<string> Selections { get; private set; }
+
+    public int SelectedItemIndex { get; set; }
+
+    public override string Value
+    {
+        get { return Selections[SelectedItemIndex]; }
+        set { TryParse(value); }
+    }
+
+    public CalcSelectionList(string name, int selectedItemIndex, IEnumerable<string> values)
+        : base(name, string.Empty, string.Empty)
+    {
+        Selections = values.ToList();
+    }
+    public CalcSelectionList(string name, string selectedItem, IEnumerable<string> values)
+    : base(name, string.Empty, string.Empty)
+    {
+        Selections = values.ToList();
+        TryParse(selectedItem);
+    }
+
+    public override bool TryParse(string strValue)
+    {
+        int i = Selections.IndexOf(strValue);
+
+        if (i == -1) { return false; }
+        else { SelectedItemIndex = i; return true; }
+
+    }
+
+    public string ValueAsString() => string.Join(", ", Selections);
+
+    public override string ToString() => string.Join(", ", Selections);
+}
